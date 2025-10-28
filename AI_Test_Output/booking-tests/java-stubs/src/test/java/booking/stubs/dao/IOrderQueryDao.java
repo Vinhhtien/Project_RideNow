adea574e43ed82c3e34030280b7dd369c2e8fd0e
vinhhtien <deptrailaphaitheok12321@gmail.com>
2025-10-29 00:30:28 +0700

    Merge code UC thông báo role par

--- AI_Test_Output/booking-tests/java-stubs/src/test/java/booking/stubs/dao/IOrderQueryDao.java
+++
@@ -1,8 +0,0 @@
-package booking.stubs.dao;
-
-import java.util.List;
-
-public interface IOrderQueryDao {
-    List<Object[]> findOrdersOfCustomerWithPaymentStatus(int customerId) throws Exception;
-}
-
--- AI_Test_Output/booking-tests/java-stubs/src/test/java/booking/stubs/service/ICustomerService.java
+++
@@ -1,9 +0,0 @@
-package booking.stubs.service;
-
-import booking.stubs.model.Customer;
-
-public interface ICustomerService {
-    Customer getProfile(int accountId) throws Exception;
-    boolean cancelOrder(int customerId, int orderId) throws Exception;
-}
-
